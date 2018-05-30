9c3c8b5dca604fca034b82ce305365f529b6f798
sergdidyk <serg.didyk@gmail.com>
2018-05-30 16:13:20 +0300

    mongoDB CRUD operations

---
+++ hw_22/server.js
@@ -0,0 +1,17 @@
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const url = "mongodb://localhost:27017/learn";
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
