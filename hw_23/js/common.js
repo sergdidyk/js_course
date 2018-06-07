70902c7e4f1b22f4fdb7e96ba516dff781551f72
sergdidyk <serg.didyk@gmail.com>
2018-06-07 16:48:53 +0300

    side changes

--- hw_23/js/common.js
+++ hw_23/js/common.js
@@ -1,3 +1,14 @@
 $(function(){
+	let preview_front = $(".side_front");
+	let preview_back = $(".side_back");
+	let main_view = $(".main_view_container");
 	
+	function changeSide(img){
+		$(img).on("click", function() {
+			let src = $(this).children().children().attr("src");
+			$(main_view).children().attr("src", src);	
+		});
+	}
+	changeSide(preview_back);
+	changeSide(preview_front);
 });
